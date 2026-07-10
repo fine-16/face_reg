3d2e7f8d6564a377ab4aebd15a96efa55a270109
fine16 <rin160513@gmail.com>
2026-07-10 11:34:18 +0900

    add camera

--- ui.py
+++ ui.py
@@ -1,6 +1,17 @@
 import os, sys, time
 import tkinter as tk
+import cv2
+from PIL import Image, ImageTk
 
 root = tk.Tk()
 root.title("My Application")
 
+CANVAS_WIDTH = 640
+CANVAS_HEIGHT = 480
+
+canvas = tk.Canvas(root, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)  
+canvas.pack()
+
+cap = cv2.VideoCapture(0)
+
+root.mainloop()
