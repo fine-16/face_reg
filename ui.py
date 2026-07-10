c8bd916e90b595035de5a634e5ffb28a757a67f0
fine16 <rin160513@gmail.com>
2026-07-10 12:02:10 +0900

    qrcode

--- ui.py
+++ ui.py
@@ -1,117 +1,49 @@
-import tkinter as tk
-from tkinter import ttk
+from time import sleep
+import tkinter
+
 import cv2
-import PIL.Image, PIL.ImageTk
-from tkinter import font
-import time
+from PIL import Image, ImageTk
+from pyzbar import pyzbar
 
+root = tkinter.Tk()
+root.title('QR reader')
+root.geometry('640x488')
 
-class Application(tk.Frame):
-    def __init__(self,master, video_source=0):
-        super().__init__(master)
+CANVAS_X = 640
+CANVAS_Y = 480
 
-        self.master.geometry("700x700")
-        self.master.title("Tkinter with Video Streaming and Capture")
+canvas = tkinter.Canvas(root, width=CANVAS_X, height=CANVAS_Y)
+canvas.pack()
 
-        # ---------------------------------------------------------
-        # Font
-        # ---------------------------------------------------------
-        self.font_frame = font.Font( family="Meiryo UI", size=15, weight="normal" )
-        self.font_btn_big = font.Font( family="Meiryo UI", size=20, weight="bold" )
-        self.font_btn_small = font.Font( family="Meiryo UI", size=15, weight="bold" )
+cap = cv2.VideoCapture(0)
 
-        self.font_lbl_bigger = font.Font( family="Meiryo UI", size=45, weight="bold" )
-        self.font_lbl_big = font.Font( family="Meiryo UI", size=30, weight="bold" )
-        self.font_lbl_middle = font.Font( family="Meiryo UI", size=15, weight="bold" )
-        self.font_lbl_small = font.Font( family="Meiryo UI", size=12, weight="normal" )
+def capture_code():
+	global CANVAS_X, CANVAS_Y
 
-        # ---------------------------------------------------------
-        # Open the video source
-        # ---------------------------------------------------------
+	ret, frame = cap.read()
+	if ret == False:
+		print("Not Image")
+	else:
+		image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+		image_pil = Image.fromarray(image_rgb)
+		image_tk = ImageTk.PhotoImage(image_pil)
+		canvas.image_tk = image_tk
+		canvas.create_image(CANVAS_X / 2, CANVAS_Y / 2, image=image_tk)
 
-        self.vcap = cv2.VideoCapture( video_source )
-        self.width = self.vcap.get( cv2.CAP_PROP_FRAME_WIDTH )
-        self.height = self.vcap.get( cv2.CAP_PROP_FRAME_HEIGHT )
+		decoded_objs = pyzbar.decode(frame)
 
-        # ---------------------------------------------------------
-        # Widget
-        # ---------------------------------------------------------
-
-        self.create_widgets()
-
-        # ---------------------------------------------------------
-        # Canvas Update
-        # ---------------------------------------------------------
-
-        self.delay = 15 #[mili seconds]
-        self.update()
-
-
-    def create_widgets(self):
-
-        #Frame_Camera
-        self.frame_cam = tk.LabelFrame(self.master, text = 'Camera', font=self.font_frame)
-        self.frame_cam.place(x = 10, y = 10)
-        self.frame_cam.configure(width = self.width+30, height = self.height+50)
-        self.frame_cam.grid_propagate(0)
-
-        #Canvas
-        self.canvas1 = tk.Canvas(self.frame_cam)
-        self.canvas1.configure( width= self.width, height=self.height)
-        self.canvas1.grid(column= 0, row=0,padx = 10, pady=10)
-
-        # Frame_Button
-        self.frame_btn = tk.LabelFrame( self.master, text='Control', font=self.font_frame )
-        self.frame_btn.place( x=10, y=550 )
-        self.frame_btn.configure( width=self.width + 30, height=120 )
-        self.frame_btn.grid_propagate( 0 )
-
-        #Snapshot Button
-        self.btn_snapshot = tk.Button( self.frame_btn, text='Snapshot', font=self.font_btn_big)
-        self.btn_snapshot.configure(width = 15, height = 1, command=self.press_snapshot_button)
-        self.btn_snapshot.grid(column=0, row=0, padx=30, pady= 10)
-
-        # Close
-        self.btn_close = tk.Button( self.frame_btn, text='Close', font=self.font_btn_big )
-        self.btn_close.configure( width=15, height=1, command=self.press_close_button )
-        self.btn_close.grid( column=1, row=0, padx=20, pady=10 )
-
-
-
-
-    def update(self):
-        #Get a frame from the video source
-        _, frame = self.vcap.read()
-
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-
-        #self.photo -> Canvas
-        self.canvas1.create_image(0,0, image= self.photo, anchor = tk.NW)
-
-        self.master.after(self.delay, self.update)
-
-    def press_snapshot_button(self):
-        # Get a frame from the video source
-        _, frame = self.vcap.read()
-
-        frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        cv2.imwrite( "frame-" + time.strftime( "%Y-%d-%m-%H-%M-%S" ) + ".jpg",
-                     cv2.cvtColor( frame1, cv2.COLOR_BGR2RGB ) )
-
-    def press_close_button(self):
-        self.master.destroy()
-        self.vcap.release()
-
-
-
-
-
-def main():
-    root = tk.Tk()
-    app = Application(master=root)#Inherit
-    app.mainloop()
-
-if __name__ == "__main__":
-    main()
+		if decoded_objs != []:
+			for obj in decoded_objs:
+				print('Type: ', obj)
+			
+				str_dec_obj = obj.data.decode('utf-8', 'ignore')
+				print('QR coed: {}'.format(str_dec_obj))
+				left, top, width, height = obj.rect
+				
+				canvas.create_rectangle(left, top, left + width, top + height, outline='green', width=5)
+				canvas.create_text(left + (width / 2), top - 30, text=str_dec_obj, font=('Helvetica', 20, 'bold'), fill='firebrick1')
+		
+		
+	canvas.after(10, capture_code)
+capture_code()
+root.mainloop()
