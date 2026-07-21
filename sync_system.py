3c1d0e0aae59a3ab3bbae393653cb1bf238b2e20
fine16 <rin160513@gmail.com>
2026-07-21 14:29:18 +0900

    change sync

--- sync_system.py
+++ sync_system.py
@@ -26,7 +26,7 @@ cmd = ["rclone", "copy", filename," MyGoogleDrive:rpi_backup"]
 
 try:
     # コマンドを実行し、完了するまで待機（エラー時は例外を発生）
-    result = subprocess.run(cmd, check=True, capture_output=True, text=True)
+    result = subprocess.run(cmd, check=True, capture_output=True, text=True,shell=True)
     print("成功:", result.stdout)
 except subprocess.CalledProcessError as e:
     print("エラーが発生しました:", e.stderr)
